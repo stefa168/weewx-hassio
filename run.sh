a425d3cb122edb290b9b40b76b873883fac91fe9
Stefano Vittorio Porta <stefa168@hotmail.it>
2022-06-23 19:34:32 +0200

    Typo

--- run.sh
+++ run.sh
@@ -7,13 +7,14 @@ if [ ! -f "${CONF_FILE}" ]; then
   echo "Configuration file missing; creating one!"
   mkdir -p $CONF_DIR
   cp /home/weewx/weewx.conf ${CONF_FILE}
-  echo "Default configuration copied into ${CONF_FILE}. Weewd will not be started this time."
+  echo "Default configuration copied into ${CONF_FILE}. Weewx will not be started this time."
   exit 1
 fi
 
-echo "Starting weewx"
+#cat /var/log/messages
 
-cat /var/log/messages
+#echo "#########################################"
+echo "Starting weewx"
 
 syslogd && /home/weewx/bin/weewxd /config/weewx/weewx.conf
 #tail -f /var/log/messages && /home/weewx/bin/weewxd /config/weewx/weewx.conf
